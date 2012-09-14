d7c887fb19f8b13f0904797c293239e1b3b38c6e
Richard Willis <willis.rh@gmail.com>
2012-09-14 01:25:13 +0100

    Finishd addpost subtask and add reset subtack

--- tasks/xen.js
+++ tasks/xen.js
@@ -1,4 +1,5 @@
 var DataStore = require('../lib/datastore');
+var fs = require('fs');
 
 module.exports = function(grunt ) {
 
@@ -8,6 +9,9 @@ module.exports = function(grunt ) {
     var config = grunt.config.get('xen');
     var task = this;
 
+    var defaultHTMLpath = grunt.task.getFile('xen/static/default-content.html');
+    var defaultHTMLsrc = grunt.file.read(defaultHTMLpath);
+
     if(!config){
       grunt.fatal('Oops! Please provide XenTask configuration in your grunt.js file');
       return;
@@ -15,8 +19,9 @@ module.exports = function(grunt ) {
 
     var getDataItem = function(type, props, uriPrefix) {
 
-      props.uri = (uriPrefix || '') + props.title.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
-      props.contentPath = props.uri + '.html';
+      var uriSegment = props.title.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+      props.uri = uriPrefix + uriSegment;
+      props.contentPath = type + '/' + uriSegment + '.html';
       props.dateCreated = (props.dateCreated === 'now') ? new Date().getTime() : props.dateCreated;
       props.showInNav = ( props.showInNav === 'y' || props.showInNav === 'Y' );
       props.tags = props.tags ? props.tags.replace(/,\s+/g, ',').split(',') : [];
@@ -37,15 +42,15 @@ module.exports = function(grunt ) {
       }, {
         name: 'view',
         message: 'View file',
-        default: 'page/generic'
+        'default': 'page/generic'
       }, {
         name: 'dateCreated',
         message: 'Date',
-        default: 'now'
+        'default': 'now'
       }, {
         name: 'showInNav',
-        message: 'Show in navigation?',
-        default: 'y/N'
+        message: 'Show in navigation? (Y/n)',
+        'default': 'y'
       }]
     };
 
@@ -54,8 +59,13 @@ module.exports = function(grunt ) {
       addpost: function(uri) {
         var done = task.async();
         grunt.helper('prompt', prompts.content, function(err, props) {
+          
           var data = getDataItem('posts', props, 'post/');
           new DataStore('posts').add(data).save();
+
+          var contentPath = './content/' + data.contentPath;
+          grunt.file.write(contentPath, defaultHTMLsrc);
+
           done();
         });
       },
@@ -63,7 +73,6 @@ module.exports = function(grunt ) {
         var done = task.async();
         grunt.helper('prompt', prompts.content, function(err, props) {
           var data = getDataItem('pages', props);
-          console.log(data, 'test');
           new DataStore('pages').add(data).save();
           done();
         });
@@ -78,6 +87,59 @@ module.exports = function(grunt ) {
           return ( post.uri === uri );
         }).remove();
       },
+      reset: function() {
+
+        function wipeDirectory(dir, callback) {
+          fs.readdir(dir, function(err, files) {
+            if (!files.length) return callback();
+            var i = 0;
+            files.forEach(function(file) {
+              var filePath = dir + '/' + file;
+              fs.stat(filePath, function(err, stats) {
+                if (err) {
+                  console.log(cd .JSON.stringify(err));
+                  return callback();
+                } 
+                if (!stats.isFile()) return callback();
+                fs.unlink(filePath, function(err) {
+                  if (err) console.log(JSON.stringify(err));
+                  if (++i === files.length) callback();
+                });
+              });
+            });
+          });  
+        }
+
+        function copyFiles(oldDir, newDir, callback) {
+          fs.readdir(oldDir, function(err, files) {
+            if (!files.length) return callback();
+            var dir = oldDir.replace(/tasks/, '');
+            files.forEach(function(file) {
+              var content = grunt.file.read(grunt.task.getFile(dir + '/' + file));
+              grunt.file.write('./' + newDir + '/' + file, content);
+            });
+            callback();
+          });
+        }
+
+        var done = task.async();
+
+        var pages = new DataStore(null, __dirname + '/xen/content/pages.json');
+        pages.save(__dirname + '/../content/pages.json')
+        
+        var posts = new DataStore(null, __dirname + '/xen/content/posts.json');
+        posts.save(__dirname + '/../content/posts.json')
+
+        wipeDirectory('content/pages', function() {
+          wipeDirectory('content/posts', function() {
+            copyFiles('tasks/xen/content/pages', 'content/pages', function() {
+              copyFiles('tasks/xen/content/posts', 'content/posts', function() {
+                done();
+              })
+            });
+          });
+        });
+      }
     };
 
     var stask = subtask[this.args[0]];
